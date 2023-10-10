a7915ae53cd9531b3561c07d2e75c154744a8b09
Catriel <72631572+perezcatriel@users.noreply.github.com>
2023-10-10 12:23:22 -0300

    Delete wp-config.php
    innecesario

--- wp-config.php
+++
@@ -1,4 +0,0 @@
-define('DB_NAME', 'woocommerce_db');
-define('DB_USER', 'woocommerce_db_user');
-define('DB_PASSWORD', 'woocommerce_db_user');
-define('DB_HOST', 'dpg-ckile4a12bvs73a16h8g-a.oregon-postgres.render.com');
